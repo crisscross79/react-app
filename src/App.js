d946747b12cabe6b6b4ef15cff6ed83a83f7a0b6
Krzysztof Żaczek <123@ss.lq>
2021-03-15 16:32:27 +0100

    send files to github

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <>
+      <div className="App">
+      <p>React app</p>
+      </div>
+      </>
+     );
+  }
+}
+ 
+export default App;
